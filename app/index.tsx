2888cad978b255a439bd15169897d8f329e49716
hakolr <m.gayvoronskiy@flang.io>
2025-03-14 16:00:12 +0300

    fix: removed logs

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
 import HomePage from '@/src/pages/HomePage'
-import React from 'react'
-import { useTranslation } from 'react-i18next'
 
 export default function HomeScreen() {
-  const { t } = useTranslation()
-
-  console.log(t('hello'))
-
   return <HomePage />
 }
